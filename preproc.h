4370c110de2298f9a8696e520e299389cef464d2
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2013-12-28 19:00:53 +0100

    Cosmetics

--- preproc.h
+++ preproc.h
@@ -4,7 +4,7 @@
 void sharedmasks(func *f1, chunk* s1, func *f2, chunk* s2);
 void reordershared(func f, var *vars);
 void shared2least(func f, chunk* m);
-void rowtocolumnmajor(func f);
+void row2columnmajor(func f);
 void pqsort(func f);
 
 #endif  /* PREPROC_H_ */

07763c594bdbdcc8eff88562762b2638ab194d44
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2013-12-29 19:51:36 +0100

    Column-major routines

--- preproc.c
+++ preproc.c
@@ -8,8 +8,8 @@ void sharedmasks(func *f1, chunk* s1, func *f2, chunk* s2) {
 	for (i = 0; i < f1->m; i++)
 		for (j = 0; j < f2->m; j++)
 			if (f1->vars[i] == f2->vars[j]) {
-				SET(s1, i);
-				SET(s2, j);
+				SET_RM(s1, i);
+				SET_RM(s2, j);
 				(f1->s)++;
 				(f2->s)++;
 				break;
@@ -17,18 +17,18 @@ void sharedmasks(func *f1, chunk* s1, func *f2, chunk* s2) {
 }
 
 __attribute__((always_inline))
-inline int compare(const void* a, const void* b, void* c)
-{
-	register func f = *(func *)c;
-	register uint8_t cmp = memcmp(a, b, sizeof(chunk) * (f.s / BITSPERCHUNK));
+inline int compare(const void* a, const void* b, void* c) {
 
-	if (cmp || !f.mask) return cmp;
+	register func f = *(func *)c;
+	register char cmp;
+	register dim i;
+	
+	for (i = 0; i < f.s / BITSPERCHUNK; i++) if ((cmp = CMP(*((chunk *)a + i), *((chunk *)b + i)))) return cmp;
+	if (!f.mask) return cmp;
 	else {
-		register chunk x = *(chunk *)a & f.mask;
-		register chunk y = *(chunk *)b & f.mask;
-		if (x == y) return 0;
-		else if (x < y) return -1;
-		else return 1;
+		register chunk x = f.mask & *((chunk *)a + f.s / BITSPERCHUNK);
+		register chunk y = f.mask & *((chunk *)b + f.s / BITSPERCHUNK);
+		return CMP(x, y);
 	}
 }
 
@@ -104,7 +104,48 @@ void shared2least(func f, chunk* m) {
 		f.vars[x] = f.vars[y];
 		f.vars[y] = t;
 		#pragma omp parallel for private(i)
-		for (i = 0; i < f.n; i++) SWAPRM(f.data + i * f.c, x, y);
+		for (i = 0; i < f.n; i++) SWAP_RM(f.data + i * f.c, x, y);
+		o[x / BITSPERCHUNK] ^= 1ULL << (x % BITSPERCHUNK);
+		a[y / BITSPERCHUNK] ^= 1ULL << (y % BITSPERCHUNK);
+	}
+
+	free(s);
+	free(z);
+	free(a);
+	free(o);
+}
+
+void shared2least_cm(func f, chunk* m) {
+
+	register dim x, y, i;
+	register var t;
+	chunk* s = (chunk *)calloc(f.c, sizeof(chunk));
+	chunk* z = (chunk *)calloc(f.c, sizeof(chunk));
+	chunk* a = (chunk *)malloc(sizeof(chunk) * f.c);
+	chunk* o = (chunk *)malloc(sizeof(chunk) * f.c);
+
+	for (i = 0; i < f.s / BITSPERCHUNK; i++) s[i] = ~(0ULL);
+	if (f.s % BITSPERCHUNK) s[f.s / BITSPERCHUNK] = f.mask;
+
+	for (i = 0; i < f.c; i++) {
+		a[i] = s[i] & ~m[i];
+		o[i] = m[i] & ~s[i];
+	}
+
+	memcpy(m, s, sizeof(chunk) * f.c);
+
+	while (memcmp(o, z, f.c * sizeof(chunk))) {
+		i = x = y = 0;
+		while (!o[i++]) x += BITSPERCHUNK;
+		x += __builtin_ctzll(o[i - 1]);
+		i = 0;
+		while (!a[i++]) y += BITSPERCHUNK;
+		y += __builtin_ctzll(a[i - 1]);
+		t = f.vars[x];
+		f.vars[x] = f.vars[y];
+		f.vars[y] = t;
+		#pragma omp parallel for private(i)
+		for (i = 0; i < f.n; i++) SWAP_CM(f.data + i, x, y, f.n);
 		o[x / BITSPERCHUNK] ^= 1ULL << (x % BITSPERCHUNK);
 		a[y / BITSPERCHUNK] ^= 1ULL << (y % BITSPERCHUNK);
 	}
@@ -125,11 +166,31 @@ void reordershared(func f, var *vars) {
 	#pragma omp parallel for private(i, s)
 	for (i = 0; i < f.n; i++) {
 		memset(s, 0, sizeof(chunk) * CEIL(f.s, BITSPERCHUNK));
-		for (j = 0; j < f.s; j++) if ((f.data[i * f.c + j / BITSPERCHUNK] >> (j % BITSPERCHUNK)) & 1) SET(s, v[f.vars[j]]);
+		for (j = 0; j < f.s; j++) if GET_RM(f.data + i * f.c, j) SET_RM(s, v[f.vars[j]]);
 		memcpy(f.data + i * f.c, s, sizeof(chunk) * (f.s / BITSPERCHUNK));
 		if (f.mask) {
 			f.data[i * f.c + f.s / BITSPERCHUNK] &= ~f.mask;
 			f.data[i * f.c + f.s / BITSPERCHUNK] |= s[f.s / BITSPERCHUNK];
+		}
+	}
+	memcpy(f.vars, vars, sizeof(var) * f.s);
+}
+
+void reordershared_cm(func f, var *vars) {
+
+	chunk s[CEIL(f.s, BITSPERCHUNK)];
+	register dim i, j;
+	var v[MAXVAR];
+
+	for (i = 0; i < f.s; i++) v[vars[i]] = i;
+	#pragma omp parallel for private(i, s)
+	for (i = 0; i < f.n; i++) {
+		memset(s, 0, sizeof(chunk) * CEIL(f.s, BITSPERCHUNK));
+		for (j = 0; j < f.s; j++) if GET_CM(f.data + i, j, f.n) SET_RM(s, v[f.vars[j]]);
+		for (j = 0; j < f.s / BITSPERCHUNK; j++) f.data[j * f.n + i] = s[j];
+		if (f.mask) {
+			f.data[(f.s / BITSPERCHUNK) * f.n + i] &= ~f.mask;
+			f.data[(f.s / BITSPERCHUNK) * f.n + i] |= s[f.s / BITSPERCHUNK];
 		}
 	}
 	memcpy(f.vars, vars, sizeof(var) * f.s);
@@ -159,15 +220,39 @@ inline void parallelmove(chunk *data, dim c, dim exp) {
 	for (j = 0; j < h; j++) move(data + 2 * j * c * k, c, k, k);
 }
 
-void row2columnmajor(func f) {
+void transpose(chunk *data, dim r, dim c) {
 
-	if (f.c > 1) {
-		register dim j, i = 0, nt = f.n, n = __builtin_popcountll(f.n);
+	if (c > 1) {
+		register dim j, i = 0, nt = r, n = __builtin_popcountll(r);
 		dim count[n];
 		while (nt) nt ^= 1ULL << (count[n - 1 - i++] = __builtin_ctzll(nt));
 		puts("Parallel moves...");
-		for (i = 0, j = 0; i < n; i++, j += 1ULL << count[i - 1]) parallelmove(f.data + j * f.c, f.c, count[i]);
+		for (i = 0, j = 0; i < n; i++, j += 1ULL << count[i - 1]) parallelmove(data + j * c, c, count[i]);
 		puts("Single moves...");
-		for (i = 1, j = 1ULL << count[0]; i < n; i++, j += 1ULL << count[i - 1]) move(f.data, f.c, j, 1ULL << count[i]);
+		for (i = 1, j = 1ULL << count[0]; i < n; i++, j += 1ULL << count[i - 1]) move(data, c, j, 1ULL << count[i]);
 	}
+}
+
+dim uniquecombinations(func f) {
+
+	register dim i, u = 1;
+
+	for (i = 1; i < f.n; i++)
+		if (memcmp(f.data + (i - 1) * f.c, f.data + i * f.c, sizeof(chunk) * (f.s / BITSPERCHUNK)) || \
+		(f.mask & (f.data[(i - 1) * f.c + f.s / BITSPERCHUNK] ^ f.data[i * f.c + f.s / BITSPERCHUNK]))) u++;
+
+	return u;
+}
+
+dim uniquecombinations_cm(func f) {
+
+	register dim i, j, u = 1;
+
+	for (i = 1; i < f.n; i++) {
+		for (j = 0; j < f.s / BITSPERCHUNK; j++) if (f.data[j * f.n + i] != f.data[j * f.n + i - 1]) { u++; goto next; }
+		if (f.mask & (f.data[(f.s / BITSPERCHUNK) * f.n + i] ^ f.data[(f.s / BITSPERCHUNK) * f.n + i - 1])) u++;
+		next:;
+	}
+
+	return u;
 }
--- preproc.h
+++ preproc.h
@@ -1,10 +1,33 @@
 #ifndef PREPROC_H_
 #define PREPROC_H_
 
+#define CMP(X, Y) ((X) == (Y) ? 0 : ((X) > (Y) ? 1 : -1))
+
+#define GET_RM(V, I) ((V)[(I) / BITSPERCHUNK] >> ((I) % BITSPERCHUNK))
+#define GET_CM(V, I, N) ((V)[((I) / BITSPERCHUNK) * (N)] >> ((I) % BITSPERCHUNK))
+
+#define SET_RM(V, I) ((V)[(I) / BITSPERCHUNK] |= 1ULL << ((I) % BITSPERCHUNK))
+#define SET_CM(V, I, N) ((V)[((I) / BITSPERCHUNK) * (N)] |= 1ULL << ((I) % BITSPERCHUNK))
+
+#define SWAP_RM(V, I, J) do { \
+	register chunk d = (GET_RM(V, I) ^ GET_RM(V, J)) & 1; \
+	(V)[(I) / BITSPERCHUNK] ^= d << ((I) % BITSPERCHUNK); (V)[(J) / BITSPERCHUNK] ^= d << ((J) % BITSPERCHUNK); } while (0)
+
+#define SWAP_CM(V, I, J, N) do { \
+        register chunk d = (GET_CM(V, I, N) ^ GET_CM(V, J, N)) & 1; \
+        (V)[((I) / BITSPERCHUNK) * (N)] ^= d << ((I) % BITSPERCHUNK); (V)[((J) / BITSPERCHUNK) * (N)] ^= d << ((J) % BITSPERCHUNK); } while (0)
+
 void sharedmasks(func *f1, chunk* s1, func *f2, chunk* s2);
+void transpose(chunk *data, dim r, dim c);
+
 void reordershared(func f, var *vars);
 void shared2least(func f, chunk* m);
-void row2columnmajor(func f);
+dim uniquecombinations(func f);
+inline int compare(const void* a, const void* b, void* c);
 void pqsort(func f);
+
+void reordershared_cm(func f, var *vars);
+void shared2least_cm(func f, chunk* m);
+dim uniquecombinations_cm(func f);
 
 #endif  /* PREPROC_H_ */

75a563c7664ad333eb27ed73fe43bc3a04842df6
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2014-02-07 22:41:02 +0100

    Values array in function definition

--- jsc.h
+++ jsc.h
@@ -28,15 +28,18 @@
 #define BITSPERCHUNK 64
 #define CPUTHREADS 8
 #define MAXVAR 200
+#define MAXVALUE 1000
 
 typedef uint64_t chunk;
 typedef uint16_t var;
 typedef uint32_t dim;
+typedef float value;
 
 typedef struct {
 	chunk *data, mask, *rmask, *hmask;
 	dim n, m, c, s, *h, hn;
 	var *vars;
+	value *v;
 } func;
 
 #ifdef ROWMAJOR

b4b7982dbcd2d21e3503167384d3355b47a7c9ed
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2015-04-30 18:42:19 +0200

    Bugs fixed

--- columnmajor.c
+++ columnmajor.c
@@ -196,15 +196,22 @@ void copymatchingrows(func *f1, func *f2, dim n1, dim n2, dim hn) {
 
         chunk *d1 = (chunk *)malloc(sizeof(chunk) * n1 * f1->c);
         chunk *d2 = (chunk *)malloc(sizeof(chunk) * n2 * f2->c);
+	value *v1 = (value *)malloc(sizeof(value) * n1);
+	value *v2 = (value *)malloc(sizeof(value) * n2);
         dim *h1 = (dim *)malloc(sizeof(dim) * hn);
         dim *h2 = (dim *)malloc(sizeof(dim) * hn);
+
+	// i1 and i2: current row in f1->data and f2->data, f1->v and f2->v
+	// i3 and i4: current row in d1 and d2, v1 and v2
 
         while (i1 != f1->n && i2 != f2->n)
                 if ((cmp = GET(f1->hmask, j1)) & GET(f2->hmask, j2)) {
                 	for (i = 0; i < f1->c; i++)
-                	memcpy(d1 + i3 + i * n1, f1->data + i1 + i * f1->n, sizeof(chunk) * f1->h[j1]); 
+                		memcpy(d1 + i3 + i * n1, f1->data + i1 + i * f1->n, sizeof(chunk) * f1->h[j1]);
                 	for (i = 0; i < f2->c; i++)
-                	memcpy(d2 + i4 + i * n2, f2->data + i2 + i * f2->n, sizeof(chunk) * f2->h[j2]); 
+                		memcpy(d2 + i4 + i * n2, f2->data + i2 + i * f2->n, sizeof(chunk) * f2->h[j2]);
+			memcpy(v1 + i3, f1->v + i1, sizeof(value) * f1->h[j1]);
+			memcpy(v2 + i4, f2->v + i2, sizeof(value) * f2->h[j2]);
                 	h1[j3++] = f1->h[j1];
                 	h2[j4++] = f2->h[j2];
                         i1 += f1->h[j1];
@@ -218,6 +225,8 @@ void copymatchingrows(func *f1, func *f2, dim n1, dim n2, dim hn) {
 
 	free(f1->data); f1->data = d1;
 	free(f2->data); f2->data = d2;
+	free(f1->v); f1->v = v1;
+	free(f2->v); f2->v = v2;
 	free(f1->h); f1->h = h1;
 	free(f2->h); f2->h = h2;
 	f1->hn = f2->hn = hn;

85f7afc24ae93da3c7a6ec46f49dcbbcfa75d15b
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2015-05-15 16:05:24 +0200

    Better way to split groups

--- jsc.cuh
+++ jsc.cuh
@@ -7,8 +7,10 @@
 #define SHAREDSIZE (44 * 1024)
 #define CONSTANTSIZE (60 * 1024)
 #define THREADSPERBLOCK 1024
-#define MEMORY(I) ((sizeof(chunk) * f1->c + sizeof(value)) * f1->h[I] + (sizeof(chunk) * f2->c + sizeof(value)) * f2->h[I] + \
-                   (sizeof(chunk) * (CEIL(f1->m + f2->m - f1->s, BITSPERCHUNK) - f1->m / BITSPERCHUNK) + sizeof(value)) * hp[I] + sizeof(dim) * 3)
+#define MEMORY(R1, R2, R3) ((sizeof(chunk) * f1->c + sizeof(value)) * (R1) + \
+			    (sizeof(chunk) * f2->c + sizeof(value)) * (R2) + \
+                   	    (sizeof(chunk) * (CEIL(f1->m + f2->m - f1->s, BITSPERCHUNK) - \
+			    f1->m / BITSPERCHUNK) + sizeof(value)) * (R3) + sizeof(dim) * 3)
 
 #define gpuerrorcheck(ans) { gpuassert((ans), __FILE__, __LINE__); }
 inline void gpuassert(cudaError_t code, const char *file, int line, bool abort = true) {

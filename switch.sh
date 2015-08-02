1d6eabb8f12d7391646a549531cf760d952bd543
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2015-08-02 13:24:55 +0200

    Cub sort

--- switch.sh
+++ switch.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+bpc=32
+
 echo -e "switch (f->c) {"
 
 for c in `seq 1 $1`; do
@@ -7,8 +9,12 @@ for c in `seq 1 $1`; do
 	echo -e "\tcase $c: {"
 	echo -e "\t\tswitch (f->s) {"
 
-	for s in `seq 1 $2`; do
-		echo -e "\t\t\tcase $s: { templatesort<chunk$c,$s>(f->data, f->v, f->n); break; }"
+	for s in `seq 1 $(( 32 * $1 - 1 ))`; do
+		echo -en "\t\t\tcase $s: { "
+		if [ $c = 1 ]
+			then echo "cubsort<$s>(f->data, f->v, f->n); break; }"
+			else echo "templatesort<chunk$c,$s>(f->data, f->v, f->n); break; }"
+		fi
 	done
 
 	echo -e "\t\t\tdefault:;\n\t\t}\n\t\tbreak;\n\t}"
